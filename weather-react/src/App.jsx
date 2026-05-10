1e7af8232a41ae11a4d9608995358d33f7d0cff8
Youssef Ahmed <moropass1212@gmail.com>
2026-05-10 14:15:05 +0300

    test

--- weather-react/src/App.jsx
+++ weather-react/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
 function App() {
-const [city , setCity] = useState("");
-const [weather , setWeather] = useState("");
-  async function fetchWeather() {
+    const [city, setCity] = useState("");
+    const [weather, setWeather] = useState("");
+    async function fetchWeather() {
         const url = `https://wttr.in/${city}?format=j1`;
 
         try {
@@ -13,31 +13,37 @@ const [weather , setWeather] = useState("");
             }
             let data = await res.json();
 
-setWeather(  data.current_condition[0].temp_C)
-
-        }
-
-        catch (error) {
+            setWeather(data.current_condition[0].temp_C);
+        } catch (error) {
             console.error("API Error:", error);
-
         }
     }
 
+    // test
 
+    return (
+        <>
+            <div className="container">
+                <label htmlFor="input" className="text">
+                    {" "}
+                    get your temperature
+                </label>
+                <input
+                    className="input"
+                    type="search"
+                    onChange={(e) => setCity(e.target.value)}
+                />
 
-  return (
-  <>
-  <div className='container'>
-    <label htmlFor="input" className='text'> get your temperature</label>
-      <input className='input' type="search" onChange={ e => setCity(e.target.value) } />
-
-<p className='text'>  the temperature at {city} is {weather}°C </p>
- <button className='weatherBtn'
-    onClick={fetchWeather}>get your city weather</button>
-</div>
-
-  </>
-  )
+                <p className="text">
+                    {" "}
+                    the temperature at {city} is {weather}°C{" "}
+                </p>
+                <button className="weatherBtn" onClick={fetchWeather}>
+                    get your city weather
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default App
